f61796e4ef1e6e9d6e7db58014e71bac00302a49
Will Blankenship <blankenship.will@gmail.com>
2015-01-23 22:30:23 +0900

    Worked on setting up the blog page, need to setup the database for blog posts and the code for displaying that. Also trying different link styles for the side-bar, currently using buttons but don't think I'll keep that.

--- MySite/settings.py
+++ MySite/settings.py
@@ -82,7 +82,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),
+					os.path.join(BASE_DIR, "blog/static/blog"),)
 
 # Template files
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"),
